8272bdc3d31d0dc28793a72a12d73d566a8f8e8c
charlie166 <charlie166@163.com>
2017-09-11 09:56:53 +0800

    添加eureka服务注册中心项目

---
+++ spring-cloud-services/spring-cloud-eureka/src/main/java/cn/charlie166/learn/spring/cloud/eureka/AppEureka.java
@@ -0,0 +1,14 @@
+package cn.charlie166.learn.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AppEureka {
+	
+    public static void main( String[] args ) {
+    	SpringApplication.run(AppEureka.class, args);
+    }
+}
